984ffdc8aa84c3ae2947745efea07ecaad2f82f2
Rithy Ngy <rithyngycambo@gmail.com>
2025-10-11 16:04:41 +0700

    Add enrollments migration

--- rxinstitute-app/database/migrations/2025_10_11_084521_create_enrollments_table.php
+++ rxinstitute-app/database/migrations/2025_10_11_084521_create_enrollments_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,12 +12,14 @@ return new class extends Migration
     {
         Schema::create('enrollments', function (Blueprint $table) {
             $table->id();
-            $table->string('enrollID');
-            $table->unsignedBigInteger('departmentID');
+            $table->string('enrollNo');
             $table->unsignedBigInteger('studentID');
+            $table->unsignedBigInteger('departmentID');
             $table->date('joinDate');
             $table->double('fee');
-            
+
+            $table->foreign('studentID')->references('id')->on('students')->onDelete('cascade');
+            $table->foreign('departmentID')->references('id')->on('departments')->onDelete('cascade');
             $table->timestamps();
         });
     }
